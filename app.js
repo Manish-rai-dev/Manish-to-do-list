a30450766f23f7641433c62562302429010de037
Manish-rai-dev <raimanishkumar52@gmail.com>
2022-01-07 02:14:48 +0530

    second commit

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-const filterOption = document.querySelector('.filter-todo');
+
+document.addEventListener('DOMcontentLoaded', getTodos);
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener('click', filterTodo);
+
 
 
 function addTodo(event) {
@@ -22,9 +23,12 @@ function addTodo(event) {
   
   todoDiv.appendChild(newTodo);
   
+saveLocalTodos(todoInput.value);
+
+
   const completedButton = document.createElement('button');
   completedButton.innerHTML = '<i class="fa fa-check"></i>';
-  completedButton.classList.add('complete-button');
+  completedButton.classList.add("complete-button");
   todoDiv.appendChild(completedButton);
 
   const trashButton = document.createElement('button');
@@ -32,7 +36,7 @@ function addTodo(event) {
   trashButton.classList.add('trash-button');
   todoDiv.appendChild(trashButton);
   todoList.appendChild(todoDiv);
-  todoInput.value = ""
+  todoInput.value = "";
 }
 
 function deleteCheck(event) {
@@ -40,35 +44,96 @@ function deleteCheck(event) {
   
   if (item.classList[0] === 'trash-button') {
     const todo = item.parentElement;
-
-    todo.classList.add('fall');
-    todo.addEventListener('transitionend', function() {
-      todo.remove();
-    });
+    todo.remove(); 
   }
 
-  if (item.classList[0] === 'complete-button') {
+  if (item.classList[0] === "complete-button") {
     const todo = item.parentElement;
-    todo.classList.toggle('completed');
+    todo.classList.toggle("completed");
   }
 
 }
 
-function filterTodo(event) {
-  const todos = todoList.childNodes;
-  todos.forEach(function(todo) {
-    switch (event.target.value) {
-      case "all":
-        todo.style.display = "flex";
-        break;
-      case "completed":
-        if (todo.classList.contains("completed")) {
-          todo.style.display = "flex";
-        } else {
-          todo.style.display = "none";
-        }
-    }
-  });
+
+function saveLocalTodos(todo)
+{
+  let todos;
+  if(localStorage.getItem("todos") === null)
+  {
+    todos=[];
+  }
+  else
+  {
+    todos= JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+
+}
+
+function getTodos()
+{
+
+
+  let todos;
+  if(localStorage.getItem("todos") === null)
+  {
+    todos=[];
+  }
+  else
+  {
+    todos= JSON.parse(localStorage.getItem("todos"));
+  }
+todos.forEach(function(todo)
+{
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  const newTodo = document.createElement('li');
+  newTodo.innerText = todo;
+  newTodo.classList.add('todo-item');
+  
+  todoDiv.appendChild(newTodo);
+
+
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fa fa-check"></i>';
+  completedButton.classList.add("complete-button");
+  todoDiv.appendChild(completedButton);
+
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fa fa-trash"></i>';
+  trashButton.classList.add('trash-button');
+  todoDiv.appendChild(trashButton);
+  todoList.appendChild(todoDiv);
+});
+}
+
+function removeLocalTodos(todo)
+{
+  let todos;
+  if(localStorage.getItem("todos") === null)
+  {
+    todos=[];
+  }
+  else
+  {
+    todos= JSON.parse(localStorage.getItem("todos"));
+  }
+    const todoIndex= todo.children[0].innerText;
+todos.splice(todos.indexOf(todoIndex),1);
+localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 
+const clear = document.querySelector(".clear");
+clear.addEventListener('click', function()
+{
+localStorage.clear();
+location.reload();
+});
+
+const dataElement = document.getElementById("date");
+let options={weekday: 'long', month: 'short', day:'numeric'};
+let today = new Date();
+dataElement.innerHTML = today.toLocaleDateString("en-US",options);
